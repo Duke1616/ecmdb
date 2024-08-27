02a95a3053d2e8e7a2a0a32ae910b8c199e3b737
luankz <17682333171@163.com>
2024-08-27 13:43:03 +0800

    ci: 测试版本信息

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Duke1616/ecmdb/cmd"
 	git "github.com/purpleclay/gitz"
 )
 
-var version string
+var (
+	version string
+)
+
+const (
+	ProjectName    = "MyAwesomeProject"
+	ProjectVersion = "1.0.0"
+	BuiltWith      = "123"             // 获取Go的版本
+	BuiltAt        = "An unknown time" // 这里可以替换为具体的构建时间
+	GitCommit      = "unknown commit"  // 这里可以替换为具体的Git提交哈希
+)
 
 func main() {
 	ver := version
+	fmt.Printf("beflore version:  %s \n", ver)
 	if version == "" {
 		ver = latestTag()
 	}
 
+	fmt.Printf("after version:  %s \n", ver)
 	cmd.Execute(ver)
 }
 

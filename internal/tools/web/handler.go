1703f9dae344ffc641b28ef689b45019079a85a8
luankz <17682333171@163.com>
2024-10-29 17:14:51 +0800

    chore: downlaod 接口

--- internal/tools/web/handler.go
+++ internal/tools/web/handler.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"fmt"
+	"github.com/Duke1616/ecmdb/pkg/ginx"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
@@ -16,6 +18,7 @@ func NewHandler() *Handler {
 func (h *Handler) PublicRoutes(server *gin.Engine) {
 	g := server.Group("/api/tools")
 	g.POST("/upload", h.Upload)
+	g.GET("/download/:filename", h.Download)
 }
 
 func (h *Handler) Upload(ctx *gin.Context) {
@@ -25,6 +28,7 @@ func (h *Handler) Upload(ctx *gin.Context) {
 		return
 	}
 
+	url := ctx.PostForm("url")
 	// 定义保存文件的路径
 	savePath := filepath.Join("uploads", file.Filename)
 
@@ -35,5 +39,29 @@ func (h *Handler) Upload(ctx *gin.Context) {
 	}
 
 	// 返回上传成功的信息
-	ctx.JSON(http.StatusOK, gin.H{"message": "文件上传成功", "url": savePath})
+	ctx.JSON(http.StatusOK, ginx.Result{
+		Data: gin.H{"message": "文件上传成功", "url": fmt.Sprintf("%s?filename=%s", url, file.Filename)},
+		Msg:  "文件上传成功",
+	})
+}
+
+func (h *Handler) Download(ctx *gin.Context) {
+	filename := ctx.Param("filename")
+
+	// 设置文件的完整路径
+	filePath := filepath.Join("uploads", filename)
+
+	// 检查文件是否存在
+	if _, err := filepath.Abs(filePath); err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "文件未找到"})
+		return
+	}
+
+	// 设置文件下载的响应头
+	ctx.Header("Content-Description", "File Transfer")
+	ctx.Header("Content-Transfer-Encoding", "binary")
+	ctx.Header("Content-Disposition", "attachment; filename="+filename)
+	ctx.Header("Content-Type", "application/octet-stream")
+
+	ctx.File(filePath)
 }

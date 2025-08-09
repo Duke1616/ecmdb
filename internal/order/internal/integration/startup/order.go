f3cf101b52b920da641b565a169a0294f232dccd
luankz <17682333171@163.com>
2025-08-10 00:04:56 +0800

    feat: order 模块编写 grpc 集成测试

--- internal/order/internal/integration/startup/order.go
+++ internal/order/internal/integration/startup/order.go
@@ -1,15 +1,11 @@
 package startup
 
 import (
-	"context"
-	jwtpkg "github.com/Duke1616/ecmdb/pkg/grpcx/interceptors/jwt"
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/spf13/viper"
 	etcdv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/naming/resolver"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/metadata"
 )
 
 func InitEcmdbClient(etcdClient *etcdv3.Client) *grpc.ClientConn {
@@ -36,23 +32,4 @@ func InitEcmdbClient(etcdClient *etcdv3.Client) *grpc.ClientConn {
 	}
 
 	return cc
-}
-
-func contextWithJWT(ctx context.Context) context.Context {
-	// 使用项目已有的JWT包创建令牌
-	jwtAuth := jwtpkg.NewJwtAuth("test_key")
-
-	// 创建包含业务ID的声明
-	claims := jwt.MapClaims{
-		"biz_id": float64(1),
-	}
-
-	// 使用JWT认证包的Encode方法生成令牌
-	tokenString, _ := jwtAuth.Encode(claims)
-
-	// 创建带有授权信息的元数据
-	md := metadata.New(map[string]string{
-		"Authorization": "Bearer " + tokenString,
-	})
-	return metadata.NewOutgoingContext(ctx, md)
 }
